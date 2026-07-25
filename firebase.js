01b4d34ac0939de2d699d3ee69b6b35210ac6fa4
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-07-25 13:14:50 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,15 +7,22 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyC1rb8p4KsKaxQK_QVtjWbFE5IStV1F5CI",
-    authDomain: "swifttrans-logistics.firebaseapp.com",
-    projectId: "swifttrans-logistics",
-    storageBucket: "swifttrans-logistics.firebasestorage.app",
-    messagingSenderId: "336087148680",
-    appId: "1:336087148680:web:d3fb690dd18d2337ca973d",
-    measurementId: "G-0W702ZGW7M"
+const const firebaseConfig = {
+  apiKey: "AIzaSyDTso8vgiWM5-K9yIyyhR0z2mqx9FyIp_w",
+  authDomain: "routewave-69c6e.firebaseapp.com",
+  projectId: "routewave-69c6e",
+  storageBucket: "routewave-69c6e.firebasestorage.app",
+  messagingSenderId: "175645333092",
+  appId: "1:175645333092:web:abde7acaa1ac4677adf171",
+  measurementId: "G-KKG9DXBZXK"
 };
+
+    
+    
+    
+
+    
+   
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
