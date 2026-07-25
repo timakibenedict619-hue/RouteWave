7ae8034ca8fcd483163d11b8c1d3afbb4379167e
timakibenedict619-hue <timakibenedict619@gmail.com>
2026-07-25 17:00:29 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDTso8vgiWM5-K9yIyyhR0z2mqx9FyIp_w",
+  authDomain: "routewave-69c6e.firebaseapp.com",
+  projectId: "routewave-69c6e",
+  storageBucket: "routewave-69c6e.firebasestorage.app",
+  messagingSenderId: "175645333092",
+  appId: "1:175645333092:web:abde7acaa1ac4677adf171",
+  measurementId: "G-KKG9DXBZXK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Authentication
+const auth = getAuth(app);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export Firebase services
+export { app, auth, db };
